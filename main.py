cbba9716d8045de34815534ff9d0209039fd822f
Sagar Deep Saha <sagarsaha@outlook.in>
2025-06-21 00:30:38 +0530

    updated requirements file

--- main.py
+++ main.py
@@ -230,5 +230,5 @@ async def get_history():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
-    # uvicorn.run(app, host="0.0.0.0", port=8000)
+    # uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
